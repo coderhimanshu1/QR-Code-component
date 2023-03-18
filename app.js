300a1f956d1d40d006172c523ca22f3da559fd8c
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-03-17 21:06:14 -0700

    updating alternate text

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function generateCard() {
   let cardHtml = `
     <div class="card shadow-lg p-3 bg-white rounded" style="max-width: 18rem;">
         <div class='card-header bg-info rounded' style='height: 250px;'>
-            <img class='card-img-top rounded mx-auto d-block m-2' src='Images/qrcode.png' alt='QR code for himanshu's portfolio' />
+            <img class='card-img-top rounded mx-auto d-block m-2' src='Images/qrcode.png' alt='scan QR code to visit my portfolio' />
         </div>
         <div class='card-body text-center'>
             <h5 class="card-title m-2">coderhimanshu.com</h5>
