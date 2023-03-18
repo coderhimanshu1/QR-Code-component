ee32863f595e645c78e32c7428b6963f2e678b69
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-03-17 20:56:15 -0700

    improved accessibility

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ card.setAttribute(
   "style",
   "display: flex; justify-content: center; align-items: center;"
 );
+
 function generateCard() {
   let cardHtml = `
     <div class="card shadow-lg p-3 bg-white rounded" style="max-width: 18rem;">
         <div class='card-header bg-info rounded' style='height: 250px;'>
-            <img class='card-img-top rounded mx-auto d-block m-2' src='Images/qrcode.png' />
+            <img class='card-img-top rounded mx-auto d-block m-2' src='Images/qrcode.png' alt='QR code for himanshu's portfolio' />
         </div>
         <div class='card-body text-center'>
             <h5 class="card-title m-2">coderhimanshu.com</h5>
